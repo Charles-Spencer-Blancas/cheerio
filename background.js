f2937f42e33a19b1fc1f9f5b4e23e97436c12dde
Charles Spencer Blancas <charles.blancas@mail.mcgill.ca>
2022-01-21 21:52:25 -0500

    Added working notifications

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
-// setInterval(() => {
-//     console.log('Hi');
-// }, 2000);
+setInterval(() => {
+    notification();
+}, 30000);
+
+function notification() {
+    chrome.notifications.create({
+        title: 'test',
+        message: 'this is a test message',
+        type: 'basic',
+        iconUrl: 'assets/128.png',
+    });
+}
