36ef73db6c5c3bca6ff10ab86a1b08e3d22091a2
Charles Spencer Blancas <charles.blancas@mail.mcgill.ca>
2022-01-22 10:02:40 -0500

    Found a way to stop and start timers

--- background.js
+++ background.js
@@ -1,22 +1,36 @@
-setInterval(
-    () =>
+function posturefunc() {
+    posture = setInterval(() => {
         notification(
             'Posture Check',
             'Are you sitting up straight?',
             'assets/128.png'
-        ),
-    minToMs(60)
-);
+        );
+    }, minToMs(60));
+}
 
-setInterval(
-    () =>
-        notification(
-            'Sip Check',
-            "Take a sip of water if you haven't in a while",
-            'assets/128.png'
-        ),
-    minToMs(30)
-);
+posturefunc();
+clearInterval(posture);
+posturefunc();
+
+// setInterval(
+//     () =>
+//         notification(
+//             'Sip Check',
+//             "Take a sip of water if you haven't in a while",
+//             'assets/128.png'
+//         ),
+//     minToMs(30)
+// );
+
+// setInterval(
+//     () =>
+//         notification(
+//             'Sip Check',
+//             "Take a sip of water if you haven't in a while",
+//             'assets/128.png'
+//         ),
+//     minToMs(30)
+// );
 
 function notification(title, message, iconURL) {
     chrome.notifications.create({
